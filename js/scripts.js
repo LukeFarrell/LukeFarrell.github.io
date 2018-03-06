1ff2f4ebab557788b163fd12328908b8f5b36737
Austin Wu <austintwu@gmail.com>
2018-03-06 15:14:44 -0500

    added cookie support I think

--- js/scripts.js
+++ js/scripts.js
@@ -127,27 +127,6 @@ $(document).ready(function() {
     var magnifVideo = function() {
         $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
             type: 'iframe',
-            // mainClass: 'mfp-fade',
-            // removalDelay: 160,
-            // preloader: false,
-            // fixedContentPos: false,
-            // closeOnBgClick: true,
-            // enableEscapeKey: true,
-            items: {
-                src: "https://www.youtube.com/watch?v=Ick-tFQuqUU"
-            },
-            // disableOn: 700,
-        });
-    };
-
-    magnifVideo();
-});
-
-$(window).load(function () {
-    // Cookies.get('kristina4dsg') !== "visited"
-    if (true){
-        $.magnificPopup.open({
-            type: 'iframe',
             mainClass: 'mfp-fade',
             removalDelay: 160,
             preloader: false,
@@ -158,17 +137,36 @@ $(window).load(function () {
                 src: "https://www.youtube.com/watch?v=Ick-tFQuqUU"
             },
             disableOn: 700,
-            callbacks: {
-                open: function() {
-                    Cookies.set('kristina4dsg', 'visited', { expires: 21 })
-                    document.body.style.overflow = "hidden";
-                },
-                close: function() {
-                    document.body.style.overflow = "visible";
-                }
-            }
-        }, 0);
-    }
-});
+        });
+    };
 
-// <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FKristinaForPresident%2Fvideos%2F2000504240199262%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+    var magnifAutoplayer = function() {
+        if (Cookies.get('kristina4dsg') !== "visited"){
+            $.magnificPopup.open({
+                type: 'iframe',
+                mainClass: 'mfp-fade',
+                removalDelay: 160,
+                preloader: false,
+                fixedContentPos: false,
+                closeOnBgClick: true,
+                enableEscapeKey: true,
+                items: {
+                    src: "https://www.youtube.com/watch?v=Ick-tFQuqUU"
+                },
+                disableOn: 700,
+                callbacks: {
+                    open: function() {
+                        Cookies.set('kristina4dsg', 'visited', { expires: 21 })
+                        document.body.style.overflow = "hidden";
+                    },
+                    close: function() {
+                        document.body.style.overflow = "visible";
+                    }
+                }
+            }, 0);
+        }
+    };
+
+    magnifVideo();
+    magnifAutoplayer();
+});
