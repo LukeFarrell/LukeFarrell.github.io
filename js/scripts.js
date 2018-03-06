0f2cd0f7a5135fe6349d1aa8c8ecaf1ba73f9996
Austin Wu <austintwu@gmail.com>
2018-03-06 15:04:07 -0500

    not working, but let me go test it online

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,51 @@
 (function($) {
-  "use strict"; // Start of use strict
+    "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 56
-  });
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 56
+    });
 
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+    // Collapse Navbar
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
 
-  // Hide navbar when modals trigger
-  $('.portfolio-modal').on('show.bs.modal', function(e) {
-    $(".navbar").addClass("d-none");
-  })
-  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
-    $(".navbar").removeClass("d-none");
-  })
+    // Hide navbar when modals trigger
+    $('.portfolio-modal').on('show.bs.modal', function(e) {
+        $(".navbar").addClass("d-none");
+    })
+    $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+        $(".navbar").removeClass("d-none");
+    })
 
 })(jQuery); // End of use strict
 
@@ -67,18 +67,18 @@ $(document).ready(function(){
         variableWidth: true,
         responsive: [
             {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-              }
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
             },
             {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
         ]
     });
@@ -86,7 +86,7 @@ $(document).ready(function(){
 
 // AOS Stuff
 AOS.init({
-  duration: 1500,
+    duration: 1500,
 })
 
 // Image Collage Stuff
@@ -121,3 +121,54 @@ $('.filters').on( 'click', 'ul.nav li a', function() {
     $container.isotope({ filter: filterValue });
     return false;
 });
+
+//Video popup
+$(document).ready(function() {
+    var magnifVideo = function() {
+        $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+            type: 'iframe',
+            // mainClass: 'mfp-fade',
+            // removalDelay: 160,
+            // preloader: false,
+            // fixedContentPos: false,
+            // closeOnBgClick: true,
+            // enableEscapeKey: true,
+            items: {
+                src: "https://www.youtube.com/watch?v=Ick-tFQuqUU"
+            },
+            // disableOn: 700,
+        });
+    };
+
+    magnifVideo();
+});
+
+$(window).load(function () {
+    // Cookies.get('kristina4dsg') !== "visited"
+    if (true){
+        $.magnificPopup.open({
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false,
+            closeOnBgClick: true,
+            enableEscapeKey: true,
+            items: {
+                src: "https://www.youtube.com/watch?v=Ick-tFQuqUU"
+            },
+            disableOn: 700,
+            callbacks: {
+                open: function() {
+                    Cookies.set('kristina4dsg', 'visited', { expires: 21 })
+                    document.body.style.overflow = "hidden";
+                },
+                close: function() {
+                    document.body.style.overflow = "visible";
+                }
+            }
+        }, 0);
+    }
+});
+
+// <iframe src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FKristinaForPresident%2Fvideos%2F2000504240199262%2F&show_text=0&width=560" width="560" height="315" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
